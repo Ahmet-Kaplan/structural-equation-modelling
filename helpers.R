232c8710747d06e45b1e31e270dde97897c2dc49
Alex <alexanderjamesryan@gmail.com>
2018-05-25 16:22:51 +0200

    changes

--- helpers.R
+++ helpers.R
@@ -1,3 +1,8 @@
+v <- list()
+
+v$fitindicies <- c("npar", "chisq", "df", "pvalue", "cfi", "rmsea", 
+                   "rmsea.ci.lower", "rmsea.ci.upper", "srmr")
+
 core_fitmeasure <- function(fit = fits$m1, fitindicies = v$fitindicies, digits = 3 ) {
   x <- fitMeasures(fit)
   round(x[fitindicies], digits)
--- working-code.R
+++ working-code.R
@@ -1,12 +1,6 @@
 # Working code
 
-v <- list()
-
-v$fitindicies <- c("npar", "chisq", "df", "pvalue", "cfi", "rmsea", 
-                   "rmsea.ci.lower", "rmsea.ci.upper", "srmr")
 
 
-
-core_fitmeasure()
 
 
